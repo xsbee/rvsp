c9737d7b421a7e97a4b243da5fb488adcd675acb
xsbee <xyesbee@protonmail.com>
2022-11-25 19:32:19 +0530

    Use realfft and put samples in sliding window

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ use std::ops::{Add, Mul, Div};
 use std::sync::Arc;
 use std::sync::Mutex;
 
-use rustfft::Fft;
-use rustfft::FftPlanner;
-use rustfft::num_complex::Complex;
-use rustfft::num_complex::ComplexFloat;
+use realfft::{RealFftPlanner, RealToComplex};
+use realfft::num_complex::{Complex, ComplexFloat};
 
 use sdl2::audio::AudioSpecDesired;
 use sdl2::audio::AudioCallback;
@@ -15,9 +13,11 @@ use sdl2::keyboard::Keycode;
 use sdl2::sys::{SDL_FRect, SDL_RenderDrawRectsF};
 
 struct FftCompute<'a> {
-    fft: Arc<dyn Fft<f32>>,
+    fft: Arc<dyn RealToComplex<f32>>,
     // shared fft buffer
     buf: &'a Mutex<Vec<Complex<f32>>>,
+    // sliding window for incoming samples
+    sliding: Vec<f32>,
     // buffer for RustFFT to work with
     scratch: Vec<Complex<f32>>,
     // von-Hann window coefficients
@@ -36,18 +36,18 @@ fn hann_window(w: &mut [f32]) {
 
 impl<'a> FftCompute<'a> {
     fn new(
-        planner: &mut FftPlanner<f32>, 
+        fft: Arc<dyn RealToComplex<f32>>, 
         buf: &'a Mutex<Vec<Complex<f32>>>, 
         len: usize
     ) -> Self {
-        let fft = planner.plan_fft_forward(len);
         let mut hann = vec![0f32; len];
 
         hann_window(&mut hann);
 
         Self {
-            scratch: vec![Complex::new(0.0, 0.0); fft.get_inplace_scratch_len()],
             buf,
+            scratch: fft.make_scratch_vec(),
+            sliding: fft.make_input_vec(),
             fft,
             hann
         }
@@ -58,14 +58,17 @@ impl<'a> AudioCallback for FftCompute<'a> {
     type Channel = f32;
 
     fn callback(&mut self, samples: &mut [Self::Channel]) {
-        let mut buf = self.buf.lock().unwrap();
+        self.sliding.drain(0..samples.len());
+        self.sliding.extend(samples.iter());
 
-        for (i, s) in samples.into_iter().enumerate() {
-            buf[i] = Complex::new(*s * self.hann[i], 0.0);
+        for (i, s) in self.sliding.iter_mut().enumerate() {
+            *s = self.hann[i] * (*s);
         }
 
-        // TODO: put samples in a sliding window prior to transform
-        self.fft.process_with_scratch(&mut buf, &mut self.scratch);
+        self.fft.process_with_scratch(
+            &mut self.sliding, 
+            &mut self.buf.lock().unwrap(), 
+            &mut self.scratch).unwrap();
     }
 }
 
@@ -84,18 +87,21 @@ fn main() {
     let sdl_video = sdl.video().unwrap();
     let mut sdl_events = sdl.event_pump().unwrap();
 
-    let mut planner = FftPlanner::new();
-    let fftbuf = Mutex::new(vec![Complex::new(0.0, 0.0); N]);
+    let mut planner = RealFftPlanner::new();
+    let fft = planner.plan_fft_forward(N);
+    let fftbuf = fft.make_output_vec();
+    let fftlen = fftbuf.len();
+    let fftbuf = Mutex::new(fftbuf);
 
     let device = sdl_audio.open_capture(
         None, 
         &AudioSpecDesired{
             channels: Some(1), 
             freq: None,
-            samples: Some(N as u16)
+            samples: None
         }, 
-        |spec|
-            FftCompute::new(&mut planner, &fftbuf, spec.samples as usize))
+        |_|
+            FftCompute::new(fft, &fftbuf, N))
     .unwrap();
 
     // TODO: make window resizable
@@ -112,18 +118,16 @@ fn main() {
         .unwrap();
 
     device.resume();
-
-    const FFT_MAXLEN: usize = N / 2 + 1; // N/2 is at nyquist freq
     
     // FIXME: use Vec<FRect> once floating points functions are introduced
-    let mut points: Vec<SDL_FRect> = vec![SDL_FRect{x: 0.0, y: 0.0, w: 0.0, h: 0.0}; FFT_MAXLEN];
+    let mut points: Vec<SDL_FRect> = vec![SDL_FRect{x: 0.0, y: 0.0, w: 0.0, h: 0.0}; fftlen];
     
     // temporally interpolated FFT output
     // https://webaudio.github.io/web-audio-api/#smoothing-over-time
-    let mut fftdata_interp: Vec<f32> = vec![0f32; FFT_MAXLEN];
+    let mut fftdata_interp: Vec<f32> = vec![0f32; fftlen];
 
     // width of each frequency bin, in display
-    let xstep = 2.0 * W as f32 / N as f32;    
+    let xstep = 2.0 * W as f32 / N as f32;
 
     'running: loop {
         for event in sdl_events.poll_iter() {
@@ -140,7 +144,7 @@ fn main() {
             canvas.set_draw_color((0, 0, 0, 255));
             canvas.clear();
 
-            for (i, bin) in fftbuf[..FFT_MAXLEN].into_iter().enumerate() {
+            for (i, bin) in fftbuf.iter().enumerate() {
                 fftdata_interp[i] = 0.3 * fftdata_interp[i] + 0.7 * (bin.abs() / N as f32); // T = 0.3
 
                 let y = fftdata_interp[i]
@@ -158,7 +162,7 @@ fn main() {
 
             unsafe {
                 // FIXME: add support for floating point functions in library
-                SDL_RenderDrawRectsF(canvas.raw(), points.as_ptr(), FFT_MAXLEN as i32);
+                SDL_RenderDrawRectsF(canvas.raw(), points.as_ptr(), fftlen as i32);
             }
 
             canvas.present();
