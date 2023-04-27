259f3fd493722822ff8d99d7b9353187e1dc993a
xsbee <xyesbee@protonmail.com>
2023-04-27 09:21:04 +0530

    Add samplerate, framerate and device selection parameters.

--- src/cli.rs
+++ src/cli.rs
@@ -8,27 +8,34 @@ use crate::utils;
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Window size of FFT in samples
-    #[arg(short = 's', long, default_value = "1024")]
+    #[arg(short = 's', long, default_value_t = 1024)]
     pub fftsize: usize,
 
+    #[arg(short = 'r', long, default_value_t = 60)]
+    pub framerate: u32,
+
     /// Width of visualizer window
-    #[arg(long, default_value = "800")]
+    #[arg(long, default_value_t = 800)]
     pub width: u32,
 
     /// Height of visualizer window
-    #[arg(long, default_value = "600")]
+    #[arg(long, default_value_t = 600)]
     pub height: u32,
 
+    /// Samplerate of audio
+    #[arg(short = 'r', long, default_value_t = 48000)]
+    pub samplerate: u32,
+
     /// Smoothing time constant (0..1)
-    #[arg(long, default_value = "0.3")]
+    #[arg(long, default_value_t = 0.8)]
     pub stc: f32,
 
     /// Minimum decibels (-Infty...0)
-    #[arg(long, default_value = "-80")]
+    #[arg(long, default_value_t = -80.0)]
     pub dbmin: f32,
 
     /// Maximum decibels (-Infty...0)
-    #[arg(long, default_value = "-20")]
+    #[arg(long, default_value_t = -20.0)]
     pub dbmax: f32,
 
     /// Color of frequency bin bars (in ARGB format)
@@ -37,5 +44,11 @@ pub struct Args {
 
     /// Color of background (in ARGB format)
     #[arg(long, default_value = "ff000000", value_parser = utils::str_to_color)]
-    pub bgcolor: Color
+    pub bgcolor: Color,
+
+    #[arg(short, long)]
+    pub device: Option<u32>,
+
+    #[arg(short, long, default_value_t = false)]
+    pub list_devices: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,44 @@ use realfft::RealFftPlanner;
 
 use const_format::formatcp;
 
+mod cli;
 mod fft;
 mod render;
-mod cli;
 mod utils;
 
 const WINDOW_TITLE: &'static str = formatcp!(
-    "{} (v{})", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    "{} (v{})",
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_VERSION")
+);
 
 fn main() {
     let args = cli::Args::parse();
 
     let sdl = sdl2::init().unwrap();
     let sdl_audio = sdl.audio().unwrap();
+
+    if args.list_devices {
+        let audio_driver = sdl_audio.current_audio_driver();
+        let num_devices = sdl_audio
+            .num_audio_capture_devices()
+            .expect("Could not enumrate devices");
+
+        println!("List of capture devices");
+        println!("-----------------------");
+        println!("Driver: {audio_driver}");
+        println!("");
+        println!("[Index]    Name");
+
+        for i in 0..num_devices {
+            let device_name = sdl_audio.audio_capture_device_name(i).unwrap();
+
+            println!("[{i:>5}]    {device_name}");
+        }
+
+        return;
+    }
+
     let sdl_video = sdl.video().unwrap();
     let mut sdl_events = sdl.event_pump().unwrap();
 
@@ -27,49 +52,62 @@ fn main() {
     let fft = planner.plan_fft_forward(args.fftsize);
     let fftlen = args.fftsize / 2 + 1;
 
-    let device = sdl_audio.open_capture(
-        None, 
-        &AudioSpecDesired {
-            channels: Some(1), 
-            freq: None,
-            // FIXME: somehow control it control FPS
-            samples: Some(512)
-        }, 
-        |_| {
-            let window = sdl_video.window(WINDOW_TITLE, args.width, args.height)
-                .position_centered()
-                .build()
-                .unwrap();
+    let device_name = args
+        .device
+        .map(|d| sdl_audio.audio_capture_device_name(d).unwrap());
 
-            let (width, height) = window.size();
-            let canvas = window
-                .into_canvas()
-                .present_vsync()
-                .accelerated()
-                .build()
-                .unwrap();
+    let device = sdl_audio
+        .open_capture(
+            device_name.as_ref().map(|d| d.as_str()),
+            &AudioSpecDesired {
+                channels: Some(1),
+                freq: Some(args.samplerate as i32),
 
-            let renderer = render::Renderer::new(
-                canvas,
-                fftlen, 
-                args.stc, 
-                args.dbmin, 
-                args.dbmax,
-                width,
-                height,
-                args.fgcolor,
-                args.bgcolor);
+                // This is the overlap of the sliding window
+                samples: Some((args.samplerate / args.framerate) as u16),
+            },
+            |_| {
+                let window = sdl_video
+                    .window(WINDOW_TITLE, args.width, args.height)
+                    .position_centered()
+                    .build()
+                    .unwrap();
 
-            fft::FftCompute::new(fft, args.fftsize, renderer)
-        }).unwrap();    
+                let (width, height) = window.size();
+                let canvas = window
+                    .into_canvas()
+                    .present_vsync()
+                    .accelerated()
+                    .build()
+                    .unwrap();
+
+                let renderer = render::Renderer::new(
+                    canvas,
+                    fftlen,
+                    args.stc,
+                    args.dbmin,
+                    args.dbmax,
+                    width,
+                    height,
+                    args.fgcolor,
+                    args.bgcolor,
+                );
+
+                fft::FftCompute::new(fft, args.fftsize, renderer)
+            },
+        )
+        .unwrap();
 
     device.resume();
 
     'running: loop {
         for event in sdl_events.poll_iter() {
             match event {
-                Event::Quit { .. } | 
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
